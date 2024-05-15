6f664c042af874d6856e60863306b7bedcc3b445
Laurent Lyaudet <laurent.lyaudet@gmail.com>
2024-05-15 12:00:04 +0200

    LL: redev overwrite_if_not_equal() bash function.

---
+++ build_and_checks_dependencies/overwrite_if_not_equal.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# This file is part of DevOrSysAdminScripts library.
+#
+# DevOrSysAdminScripts is free software:
+# you can redistribute it and/or modify it under the terms
+# of the GNU Lesser General Public License
+# as published by the Free Software Foundation,
+# either version 3 of the License,
+# or (at your option) any later version.
+#
+# DevOrSysAdminScripts is distributed in the hope
+# that it will be useful,
+# but WITHOUT ANY WARRANTY;
+# without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of
+# the GNU Lesser General Public License
+# along with DevOrSysAdminScripts.
+# If not, see <https://www.gnu.org/licenses/>.
+#
+# ©Copyright 2023-2024 Laurent Lyaudet
+
+overwrite_if_not_equal(){
+  # $1=$target_file_name
+  # $2=$temp_file_name
+  is_equal=$(diff -q "$1" "$2")
+  if [ is_equal == 0 ]; then
+    rm "$2"
+  else
+    mv "$2" "$1"
+  fi
+}

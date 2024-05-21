24402f641c47e69f0248a16ec63a2f04bbf27859
Laurent Lyaudet <laurent.lyaudet@gmail.com>
2024-05-21 21:42:08 +0200

    LL: correction of post_commit_renaming_comment.libr.sh when multiple files are renamed in same commit; factorization of timestamp; shellcheck.

--- post_commit_renaming_comment.libr.sh
+++ post_commit_renaming_comment.libr.sh
@@ -36,6 +36,7 @@ commit_a_file_renamed_comment(){
     LFBFL_verbose=1
   fi
   readonly LFBFL_verbose
+  # shellcheck disable=SC2155
   declare -r LFBFL_renaming_lines=$(
     git log -p -1 | grep 'diff --git' -A 1
   )
@@ -48,16 +49,23 @@ commit_a_file_renamed_comment(){
   mapfile -t LFBFL_renaming_lines_array <<< "${LFBFL_renaming_lines}"
   readonly LFBFL_renaming_lines_array
 
+  # shellcheck disable=SC2155
+  declare -r LFBFL_timestamp=$(date --iso-8601="minute")
+  # shellcheck disable=SC2250
+  if [[ $LFBFL_verbose -eq 1 ]]; then
+    echo "LFBFL_timestamp: ${LFBFL_timestamp}"
+  fi
   declare -i LFBFL_renaming_happened=0
   declare -i LFBFL_i
   for ((LFBFL_i=0; LFBFL_i<${#LFBFL_renaming_lines_array[@]};)); do
     # shellcheck disable=SC2250
-    declare -r\
-      LFBFL_diff_line="${LFBFL_renaming_lines_array[$LFBFL_i]}"
+    local LFBFL_diff_line="${LFBFL_renaming_lines_array[$LFBFL_i]}"
     LFBFL_i=$((LFBFL_i + 1))
     # shellcheck disable=SC2250
-    declare -r\
+    local\
       LFBFL_similarity_line="${LFBFL_renaming_lines_array[$LFBFL_i]}"
+    LFBFL_i=$((LFBFL_i + 1))
+    # We need to increment 3 times since grep adds "--" lines between.
     LFBFL_i=$((LFBFL_i + 1))
     # shellcheck disable=SC2250
     if [[ $LFBFL_verbose -eq 1 ]]; then
@@ -66,17 +74,19 @@ commit_a_file_renamed_comment(){
     fi
     if [[ "${LFBFL_similarity_line:17:4}" == "100%" ]]; then
       # Extract old file name from diff line. ------------------------
-      declare -r LFBFL_old_file_path=$(
+      # shellcheck disable=SC2155
+      local LFBFL_old_file_path=$(
         echo "${LFBFL_diff_line}" | cut -d ' ' -f 3
       )
-      declare -r\
-        LFBFL_old_file_name=$(basename "${LFBFL_old_file_path}")
+      # shellcheck disable=SC2155
+      local LFBFL_old_file_name=$(basename "${LFBFL_old_file_path}")
       # Extract new file name from diff line. ------------------------
-      declare -r LFBFL_new_file_path=$(
+      # shellcheck disable=SC2155
+      local LFBFL_new_file_path=$(
         echo "${LFBFL_diff_line}" | cut -d ' ' -f 4
       )
-      declare -r\
-        LFBFL_new_file_name=$(basename "${LFBFL_new_file_path}")
+      # shellcheck disable=SC2155
+      local LFBFL_new_file_name=$(basename "${LFBFL_new_file_path}")
       # shellcheck disable=SC2250
       if [[ $LFBFL_verbose -eq 1 ]]; then
         echo "LFBFL_old_file_path: ${LFBFL_old_file_path}"
@@ -90,7 +100,6 @@ commit_a_file_renamed_comment(){
       if [[ "${LFBFL_useful_file_name:-4}" == ".tpl" ]]; then
         LFBFL_useful_file_name="${LFBFL_useful_file_name:0:-4}"
       fi
-      readonly LFBFL_useful_file_name
       local LFBFL_comment_prefix=""
       if [[ "${useful_file_name:-3}" == ".sh" ]]; then
         LFBFL_comment_prefix="# "
@@ -101,14 +110,13 @@ commit_a_file_renamed_comment(){
       if [[ "${useful_file_name:-4}" == ".tex" ]]; then
         LFBFL_comment_prefix="% "
       fi
-      readonly LFBFL_comment_prefix
       # shellcheck disable=SC2250
       if [[ $LFBFL_verbose -eq 1 ]]; then
         echo "LFBFL_comment_prefix: ${LFBFL_comment_prefix}"
       fi
       # Create new comment string. -----------------------------------
       local LFBFL_new_comment="${LFBFL_comment_prefix}"
-      LFBFL_new_comment+=$(date --iso-8601="minute")
+      LFBFL_new_comment+="${LFBFL_timestamp}"
       LFBFL_new_comment+=": This file was renamed from\n"
       LFBFL_new_comment+="${LFBFL_comment_prefix}"
       LFBFL_new_comment+='"'"${LFBFL_old_file_name}"'"\n'
@@ -121,16 +129,18 @@ commit_a_file_renamed_comment(){
         echo "LFBFL_new_comment: ${LFBFL_new_comment}"
       fi
       # Find line with ©Copyright. -----------------------------------
-      declare -ir LFBFL_copyright_line_number=$(
-        grep -n '©Copyright' "${LFBFL_new_file_name}" | cut -d ':' -f 1
+      # shellcheck disable=SC2155
+      declare -i LFBFL_copyright_line_number=$(
+        grep -n '©Copyright' "${LFBFL_new_file_name}"\
+        | cut -d ':' -f 1
       )
       # shellcheck disable=SC2250
       if [[ $LFBFL_verbose -eq 1 ]]; then
-        echo "LFBFL_copyright_line_number: ${LFBFL_copyright_line_number}"
+        echo "LFBFL_copyright_line_number:"\
+             " ${LFBFL_copyright_line_number}"
       fi
       if [[ ${LFBFL_copyright_line_number} -eq 0 ]]; then
-        declare -r\
-          LFBFL_send_summary_2="ATTENTION: No line with copyright"
+        local LFBFL_send_summary_2="ATTENTION: No line with copyright"
         notify-send "${LFBFL_send_summary_2}"\
           "${LFBFL_send_summary_2} for: ${LFBFL_new_comment}"
         # shellcheck disable=SC2250
@@ -140,8 +150,8 @@ commit_a_file_renamed_comment(){
         continue
       fi
       # Get total number of lines. -----------------------------------
-      # shellcheck disable=SC2002
-      declare -ir LFBFL_line_count=$(
+      # shellcheck disable=SC2002,SC2155
+      declare -i LFBFL_line_count=$(
         cat "${LFBFL_new_file_name}" | wc -l
       )
       # shellcheck disable=SC2250
@@ -149,7 +159,8 @@ commit_a_file_renamed_comment(){
         echo "LFBFL_line_count: ${LFBFL_line_count}"
       fi
       # Compute number of lines after. -------------------------------
-      declare -ir LFBFL_lines_after=$((
+      # shellcheck disable=SC2155
+      declare -i LFBFL_lines_after=$((
         LFBFL_line_count - LFBFL_copyright_line_number
       ))
       # shellcheck disable=SC2250
@@ -157,8 +168,7 @@ commit_a_file_renamed_comment(){
         echo "LFBFL_lines_after: ${LFBFL_lines_after}"
       fi
       # Update file. -------------------------------------------------
-      declare -r\
-        LFBFL_temp_file_name="${LFBFL_new_file_name}.LFBFL.temp"
+      local LFBFL_temp_file_name="${LFBFL_new_file_name}.LFBFL.temp"
       head --lines="${LFBFL_copyright_line_number}"\
         "${LFBFL_new_file_name}" > "${LFBFL_temp_file_name}"
       echo -e "${LFBFL_new_comment}" >> "${LFBFL_temp_file_name}"
@@ -173,7 +183,9 @@ commit_a_file_renamed_comment(){
   readonly LFBFL_renaming_happened
   if [[ LFBFL_renaming_happened -eq 1 ]]; then
     # Commit ---------------------------------------------------------
-    git commit\
-      --message="commit_a_file_renamed_comment() post-commit hook."
+    local LFBFL_message="commit_a_file_renamed_comment()"
+    LFBFL_message+=" post-commit hook, ${LFBFL_timestamp}."
+    readonly LFBFL_message
+    git commit --message="${LFBFL_message}"
   fi
 }

543c8e4876f4aac91013249629b5f363d83cd825
Laurent Lyaudet <laurent.lyaudet@gmail.com>
2024-06-07 16:49:27 +0200

    LL: re build_licenses_templates.exec.sh without shellcheck warnings and with better code conventions.

--- build_and_checks_dependencies/licenses_templates/build_licenses_templates.exec.sh
+++ build_and_checks_dependencies/licenses_templates/build_licenses_templates.exec.sh
@@ -24,24 +24,25 @@
 # This file was renamed from "build_licenses_templates.sh"
 # to "build_licenses_templates.exec.sh".
 
-verbose=""
+LFBFL_verbose=""
 if [[ "$1" == "--verbose" ]]; then
   echo "$0 $*"
-  verbose="--verbose"
+  # shellcheck disable=SC2034
+  LFBFL_verbose="--verbose"
 fi
 
-subdir="build_and_checks_dependencies"
+LFBFL_subdir="build_and_checks_dependencies"
 # shellcheck disable=SC1090
-source "./${subdir}/comparisons.libr.sh"
+source "./${LFBFL_subdir}/comparisons.libr.sh"
 # shellcheck disable=SC1090
-source "./${subdir}/generate_from_template.libr.sh"
+source "./${LFBFL_subdir}/generate_from_template.libr.sh"
 # shellcheck disable=SC1090
-source "./${subdir}/lines_filters.libr.sh"
+source "./${LFBFL_subdir}/lines_filters.libr.sh"
 # shellcheck disable=SC1090
-source "./${subdir}/overwrite_if_not_equal.libr.sh"
+source "./${LFBFL_subdir}/overwrite_if_not_equal.libr.sh"
 
-license_subdir="./${subdir}/licenses_templates/"
-license_prefix="${license_subdir}license_file_header_"
+LFBFL_license_subdir="./${LFBFL_subdir}/licenses_templates/"
+LFBFL_license_prefix="${LFBFL_license_subdir}license_file_header_"
 licenses=(\
   "GPLv3+"\
   "LGPLv3+"
@@ -64,7 +65,9 @@ block_comment_exits=(\
 equal "${#block_comment_languages[@]}"\
       "${#block_comment_enters[@]}"\
       "${#block_comment_exits[@]}"
+# shellcheck disable=SC2181
 if [[ $? == 0 ]]; then
+  # shellcheck disable=SC1003
   echo '/!\'"Problème de définition des tableaux de langages 1"'/!\'
 fi
 
@@ -78,7 +81,9 @@ line_comment_prefixes=(\
 )
 equal "${#line_comment_languages[@]}"\
       "${#line_comment_prefixes[@]}"
+# shellcheck disable=SC2181
 if [[ $? == 0 ]]; then
+  # shellcheck disable=SC1003
   echo '/!\'"Problème de définition des tableaux de langages 2"'/!\'
 fi
 # --------------------------------------------------------------------
@@ -89,77 +94,81 @@ fi
 # Kamoulox do endfor, Jacques Beauregard XD
 # (C'est toujours mieux que l'inverse :) !)
 # for do endfor -> for do done (Fort Doux Donne).
-for license in "${licenses[@]}"; do
-  license_prefix2="$license_prefix$license"
+for LFBFL_license in "${licenses[@]}"; do
+  LFBFL_license_prefix2="${LFBFL_license_prefix}${LFBFL_license}"
   for ((i=0; i<${#block_comment_languages[@]}; i++)); do
-    extension=${block_comment_languages[i]}
-    license_file_name="$license_prefix2.$extension"
-    enter_string=${block_comment_enters[i]}
-    exit_string=${block_comment_exits[i]}
+    LFBFL_extension=${block_comment_languages[i]}
+    license_file_name="${LFBFL_license_prefix2}.${LFBFL_extension}"
+    LFBFL_enter_string=${block_comment_enters[i]}
+    LFBFL_exit_string=${block_comment_exits[i]}
     generate_from_template_with_block_comments\
-      "$license_prefix2.tpl"\
-      "$license_file_name.tpl"\
-      "$enter_string" "$exit_string"
+      "${LFBFL_license_prefix2}.tpl"\
+      "${license_file_name}.tpl"\
+      "${LFBFL_enter_string}" "${LFBFL_exit_string}"
   done
   LFBFL_temp2=".generate_from_template_with_line_comments.temp"
   for ((i=0; i<${#line_comment_languages[@]}; i++)); do
-    extension=${line_comment_languages[i]}
-    license_file_name="$license_prefix2.$extension"
-    prefix_string=${line_comment_prefixes[i]}
-    
+    LFBFL_extension=${line_comment_languages[i]}
+    license_file_name="${LFBFL_license_prefix2}.${LFBFL_extension}"
+    LFBFL_prefix_string=${line_comment_prefixes[i]}
+    LFBFL_intermediate_file_name="${license_file_name}.tpl"
+    LFBFL_intermediate_file_name+="${LFBFL_temp2}"
     generate_from_template_with_line_comments\
-      "$license_prefix2.tpl"\
-      "$license_file_name.tpl"\
-      "$prefix_string"\
-      "sed -i -E -e 's/\s*$//g' '$license_file_name.tpl$LFBFL_temp2'"
+      "${LFBFL_license_prefix2}.tpl"\
+      "${license_file_name}.tpl"\
+      "${LFBFL_prefix_string}"\
+      "sed -Ei -e 's/\s*$//g' '${LFBFL_intermediate_file_name}'"
   done
 done
 # --------------------------------------------------------------------
 
-file_name=repository_data.txt
-grep_variable "$file_name" repository_name
-grep_variable "$file_name" license
-grep_variable "$file_name" author_full_name
-license_prefix2="$license_prefix$license"
+LFBFL_file_name=repository_data.txt
+repository_name=""
+license=""
+author_full_name=""
+grep_variable "${LFBFL_file_name}" repository_name
+grep_variable "${LFBFL_file_name}" license
+grep_variable "${LFBFL_file_name}" author_full_name
+LFBFL_license_prefix2="${LFBFL_license_prefix}$license"
 # First year according to current state of git repository.
 first_year="$(git log | grep 'Date:' | cut -f 8 -d ' ' | tail -1)"
 # Last year according to current state of git repository.
 last_year="$(git log | grep 'Date:' | cut -f 8 -d ' ' | head -1)"
-copyright_string="$first_year-$last_year $author_full_name"
+copyright_string="${first_year}-${last_year} ${author_full_name}"
 for ((i=0; i<${#block_comment_languages[@]}; i++)); do
-  extension=${block_comment_languages[i]}
-  license_file_name="$license_prefix2.$extension"
-  sed -e "s/@repository_name@/$repository_name/g"\
-    -e "s/@copyright_string@/$copyright_string/g"\
-    "$license_file_name.tpl" > "$license_file_name.temp"
-  overwrite_if_not_equal "$license_file_name"\
-    "$license_file_name.temp"
-  head --lines=-1 "$license_file_name" | tail --lines=-1\
-    > "$license_file_name.temp"
-  find . -type f -name "*.$extension" -printf '%P\n' | relevant_find\
-    | while read -r file_name;
+  LFBFL_extension=${block_comment_languages[i]}
+  license_file_name="${LFBFL_license_prefix2}.${LFBFL_extension}"
+  sed -e "s/@repository_name@/${repository_name}/g"\
+    -e "s/@copyright_string@/${copyright_string}/g"\
+    "${license_file_name}.tpl" > "${license_file_name}.temp"
+  overwrite_if_not_equal "${license_file_name}"\
+    "${license_file_name}.temp"
+  head --lines=-1 "${license_file_name}" | tail --lines=-1\
+    > "${license_file_name}.temp"
+  find . -type f -name "*.${LFBFL_extension}" -printf '%P\n'\
+    | relevant_find | while read -r LFBFL_file_name;
   do
-    if is_subfile "$file_name" "$license_file_name.temp"
+    if is_subfile "${LFBFL_file_name}" "${license_file_name}.temp"
     then
-      echo "File $file_name has no/wrong license header?"
+      echo "File ${LFBFL_file_name} has no/wrong license header?"
     fi
   done
-  rm "$license_file_name.temp"
+  rm "${license_file_name}.temp"
 done
 for ((i=0; i<${#line_comment_languages[@]}; i++)); do
-  extension=${line_comment_languages[i]}
-  license_file_name="$license_prefix2.$extension"
-  sed -e "s/@repository_name@/$repository_name/g"\
-    -e "s/@copyright_string@/$copyright_string/g"\
-    "$license_file_name.tpl" > "$license_file_name.temp"
-  overwrite_if_not_equal "$license_file_name"\
-    "$license_file_name.temp"
-  find . -type f -name "*.$extension" -printf '%P\n' | relevant_find\
-    | while read -r file_name;
+  LFBFL_extension=${line_comment_languages[i]}
+  license_file_name="${LFBFL_license_prefix2}.${LFBFL_extension}"
+  sed -e "s/@repository_name@/${repository_name}/g"\
+    -e "s/@copyright_string@/${copyright_string}/g"\
+    "${license_file_name}.tpl" > "${license_file_name}.temp"
+  overwrite_if_not_equal "${license_file_name}"\
+    "${license_file_name}.temp"
+  find . -type f -name "*.${LFBFL_extension}" -printf '%P\n'\
+    | relevant_find | while read -r LFBFL_file_name;
   do
-    if is_subfile "$file_name" "$license_file_name"
+    if is_subfile "${LFBFL_file_name}" "${license_file_name}"
     then
-      echo "File $file_name has no/wrong license header?"
+      echo "File ${LFBFL_file_name} has no/wrong license header?"
     fi
   done
 done

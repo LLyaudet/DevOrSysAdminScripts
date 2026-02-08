445d85336d29850d860018654c10778d33d232c2
Laurent Lyaudet <laurent.lyaudet@gmail.com>
2026-02-08 12:22:12 +0100

    LL: some verbose messages in build_md_from_printable_md.exec.sh.

--- build_and_checks_dependencies/build_md_from_printable_md.exec.sh
+++ build_and_checks_dependencies/build_md_from_printable_md.exec.sh
@@ -45,6 +45,9 @@ build_md_from_printable_md(){
   pushd .
   if [[ -n "$1" ]];
   then
+    if [[ $LFBFL_verbose -eq 1 ]]; then
+      echo "Moving to directory: $1"
+    fi
     cd "$1" || {
       LFBFL_cd_result=$?;
       echo "build_md_from_printable_md no such directory";
@@ -56,6 +59,9 @@ build_md_from_printable_md(){
   file_name="README"
   if [[ -n "$2" ]];
   then
+    if [[ $LFBFL_verbose -eq 1 ]]; then
+      echo "Searching md file: $2"
+    fi
     file_name="$2"
   fi
 

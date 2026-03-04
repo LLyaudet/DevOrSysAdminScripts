657a2e495948fcc2aa3286f2e490c75312f38e12
Laurent Lyaudet <laurent.lyaudet@gmail.com>
2026-03-04 17:55:29 +0100

    LL: too_long_code_lines.libr.sh correction trailing backslash works with --fixed-strings.

--- build_and_checks_dependencies/too_long_code_lines.libr.sh
+++ build_and_checks_dependencies/too_long_code_lines.libr.sh
@@ -78,7 +78,7 @@ too_long_code_lines(){
     do
       LFBFL_file_name=${LFBFL_long_line%%:*} # Drop long ':*' suffix
       LFBFL_line=${LFBFL_long_line#*:} # Drop short '*:' prefix
-      LFBFL_line="${LFBFL_line/%\\/}" # Drop/Replace '\' suffix by ''
+      # LFBFL_line="${LFBFL_line/%\\/}" # Drop/Replace '\' suffix by ''
       LFBFL_extension=${LFBFL_file_name##*.} # Drop long '*.' prefix
       LFBFL_base_name=${LFBFL_file_name%.*} # Drop short '.*' suffix
       if [[ "${LFBFL_extension}" == "html" ]]; then

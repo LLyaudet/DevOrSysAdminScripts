91ae10412b54b074a46147375eb6d0a063c5b93f
Laurent Lyaudet <laurent.lyaudet@gmail.com>
2026-03-23 18:38:12 +0100

    LL: wget_sha512.libr.sh with --check-download or --check-download=keep option.

--- wget_sha512.libr.sh
+++ wget_sha512.libr.sh
@@ -28,13 +28,24 @@ wget_sha512(){
   # $1=file_path
   # $2=download_URL
   # $3=correct_sha512
-  # $4=optional --verbose
+  # Options:
+  #   --check-download = keep to keep the downloaded file
+  #   --verbose
   local LFBFL_verbose=""
   if [[ "$*" == *--verbose* ]]; then
     printf "%s wget_sha512 %s\n" "$0" "$*"
     LFBFL_verbose="--verbose"
   fi
   readonly LFBFL_verbose
+  local LFBFL_check_download=""
+  if [[ "$*" == *--check-download=keep* ]]; then
+    LFBFL_check_download="--check-download=keep"
+  elif [[ "$*" == *--check-download* ]]; then
+    LFBFL_check_download="--check-download"
+  fi
+  readonly LFBFL_check_download
+  declare -i LFBFL_i_error=0
+
   if [[ ! -f "$1" ]]; then
     wget ${LFBFL_verbose:+"${LFBFL_verbose}"} -O "$1" "$2"
   fi
@@ -45,6 +56,24 @@ wget_sha512(){
   if [[ "${LFBFL_present_sha512}" != "$3" ]]; then
     printf "%s does not have correct sha512:\n - wanted %s\n - found %s\n"\
       "$1" "$3" "${LFBFL_present_sha512}"
-    return 1
+    LFBFL_i_error=1
   fi
+  if [[ -n "${LFBFL_check_download}" ]]; then
+    wget ${LFBFL_verbose:+"${LFBFL_verbose}"} -O "$1.wget_sha512.temp" "$2"
+    declare -r LFBFL_present_sha512_2=$(
+      sha512sum "$1.wget_sha512.temp"\
+      | cut --fields=1 --delimiter=' '
+    )
+    if [[ "${LFBFL_check_download}" != "--check-download=keep" ]]; then
+      rm "$1.wget_sha512.temp"
+    fi
+    if [[ "${LFBFL_present_sha512_2}" != "$3" ]]; then
+      printf\
+        "%s does not have correct sha512:\n - wanted %s\n - found %s\n"\
+        "Online version of $1" "$3" "${LFBFL_present_sha512_2}"
+      LFBFL_i_error=1
+    fi
+  fi
+  # shellcheck disable=SC2248
+  return ${LFBFL_i_error}
 }

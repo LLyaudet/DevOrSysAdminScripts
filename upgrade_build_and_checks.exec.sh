9b7dc976938d029c7fc6b914db01bb27e70c81d4
Laurent Lyaudet <laurent.lyaudet@gmail.com>
2026-04-01 19:17:28 +0200

    LL: upgrade_build_and_checks.exec.sh with -- arguments splitting on all relevant places.

--- upgrade_build_and_checks.exec.sh
+++ upgrade_build_and_checks.exec.sh
@@ -31,7 +31,7 @@ upgrade_build_and_checks(){
   #   --verbose
   local LFBFL_verbose=""
   if [[ "$*" == *--verbose* ]]; then
-    printf "%s %s\n" "$0" "$*"
+    printf -- "%s %s\n" "$0" "$*"
     LFBFL_verbose="--verbose"
   fi
   local LFBFL_fixed_point_build=""

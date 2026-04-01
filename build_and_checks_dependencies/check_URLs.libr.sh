a6172056d232cad73381625608c4d04b82e98521
Laurent Lyaudet <laurent.lyaudet@gmail.com>
2026-04-01 20:50:40 +0200

    LL: check_URLs.libr.sh with -- arguments splitting on all relevant places.

--- build_and_checks_dependencies/check_URLs.libr.sh
+++ build_and_checks_dependencies/check_URLs.libr.sh
@@ -90,14 +90,16 @@ check_URLs(){
       [[ "${LFBFL_base_file_name}" != "check_URLs.libr.sh" ]]\
         || continue
       if [[ LFBFL_i_verbose -eq 1 ]]; then
-        printf "check_URLs: Handling the file: %s.\n" "${LFBFL_file_path}"
+        printf -- "check_URLs: Handling the file: %s.\n"\
+          "${LFBFL_file_path}"
       fi
       for LFBFL_substitution in "${!LFBFL_substitutions[@]}"; do
         LFBFL_substitution2=${LFBFL_substitutions[${LFBFL_substitution}]}
-        if grep --quiet "${LFBFL_substitution}" "${LFBFL_file_path}"; then
+        if grep --quiet "${LFBFL_substitution}" -- "${LFBFL_file_path}";
+        then
           sed --in-place\
-            "s|${LFBFL_substitution}|${LFBFL_substitution2}|g"\
-            "${LFBFL_file_path}"
+          --expression="s|${LFBFL_substitution}|${LFBFL_substitution2}|g"\
+            -- "${LFBFL_file_path}"
         fi
       done
     done
